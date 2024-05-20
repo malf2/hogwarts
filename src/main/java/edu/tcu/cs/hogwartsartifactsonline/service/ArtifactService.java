332adf3c82f02de9e69074a3b8ff1a571f3c9c45
Matheus De Lima Freire <matheus.delimafreire@outlook.com>
2024-05-20 20:42:26 +0100

    Artifacts CRUD done, close #1

---
+++ src/main/java/edu/tcu/cs/hogwartsartifactsonline/service/ArtifactService.java
@@ -0,0 +1,48 @@
+package edu.tcu.cs.hogwartsartifactsonline.service;
+
+import edu.tcu.cs.hogwartsartifactsonline.dao.ArtifactDao;
+import edu.tcu.cs.hogwartsartifactsonline.domain.Artifact;
+import javax.transaction.Transactional;
+
+import edu.tcu.cs.hogwartsartifactsonline.domain.Result;
+import edu.tcu.cs.hogwartsartifactsonline.utils.IdWorker;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+
+@Service
+@Transactional
+public class ArtifactService {
+
+    private ArtifactDao artifactDao;
+    private IdWorker idWorker;
+
+    public ArtifactService(ArtifactDao artifactDao, IdWorker idWorker) {
+        this.artifactDao = artifactDao;
+        this.idWorker = idWorker;
+    }
+
+    public List<Artifact> findAll() {
+        return artifactDao.findAll();
+    }
+
+    public Artifact findById(String artifactId) {
+        return artifactDao.findById(artifactId).get();
+    }
+
+    public void save(Artifact newArtifact) {
+        newArtifact.setId(idWorker.nextId() + "");
+        artifactDao.save(newArtifact);
+    }
+
+    public void update(String artifactId, Artifact updatedArtifact) {
+        updatedArtifact.setId(artifactId);
+        artifactDao.save(updatedArtifact);
+    }
+
+    public void delete(String artifactId) {
+        artifactDao.deleteById(artifactId);
+    }
+}
